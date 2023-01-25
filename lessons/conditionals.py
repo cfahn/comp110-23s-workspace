7bfa11654c4077b880c2dfe71122d51923fcd393
Catherine Ahn <cfahn@ad.unc.edu>
2023-01-25 16:03:26 -0500

    conditionals practice

---
+++ lessons/conditionals.py
@@ -0,0 +1,13 @@
+"""If it's raining. tell me to pack umbrella"""
+
+weather: str = input("What is the weather like? ")
+
+if (weather == "rain"):
+    print("Pack an umbrella!")
+    print("But splash in the puddles!")
+
+else:
+    if (weather == "snow"):
+        print("Pack a jacket!")
+    print("You don't ave an umbrella!")
+print("Have a lovely day")
